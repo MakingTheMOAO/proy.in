4fe98d813014a0a492cb021a619d14c37ef3e48b
MakingTheMOAO <tisera.emilianodavid@gmail.com>
2023-03-19 18:27:21 -0300

    commit final

--- Etl_script.py
+++ Etl_script.py
@@ -1,9 +1,8 @@
-# %%
+
 # Importar librerias necesarias.
 import pandas as pd 
 import numpy as np
 
-# %%
 # Cargar los Datasets
 # Ratins:
 df1 = pd.read_csv(r"ratings\1.csv")
@@ -18,34 +17,27 @@ df8 = pd.read_csv(r"ratings\8.csv")
 
 
 
-# %%
 # Csv de plataformas:
 amazon = pd.read_csv(r"amazon_prime_titles.csv")
 disney = pd.read_csv(r"disney_plus_titles.csv")
 hulu = pd.read_csv(r"hulu_titles.csv")
 netflix = pd.read_csv(r"netflix_titles.csv")
 
-# %% [markdown]
 # Proceso de ETL:
 
-# %%
-amazon.head()
-
-# %%
 # ajustar los nombres de las id
 amazon.show_id = "a" + amazon.show_id
 disney.show_id = "d" + disney.show_id
 hulu.show_id = "h" + hulu.show_id
 netflix.show_id = "n" + netflix.show_id
 
-# %%
 # definir celdas NaN
 amazon.rating.fillna("G", inplace=True)
 disney.rating.fillna("G", inplace=True)
 hulu.rating.fillna("G", inplace=True)
 netflix.rating.fillna("G", inplace=True)
 
-# %%
+
 # reformatear a datetimes yyyy-mm-dd
 amazon["date_added"] = pd.to_datetime(amazon["date_added"])
 disney["date_added"] = pd.to_datetime(disney["date_added"])
@@ -54,7 +46,7 @@ netflix["date_added"] = pd.to_datetime(netflix["date_added"])
 
 
 
-# %%
+
 # aplicar lower case a todo lo que sea texto
 amazon = amazon.applymap(lambda s: s.lower() if type(s) == str else s)
 disney = disney.applymap(lambda s: s.lower() if type(s) == str else s)
@@ -62,21 +54,38 @@ hulu = hulu.applymap(lambda s: s.lower() if type(s) == str else s)
 netflix = netflix.applymap(lambda s: s.lower() if type(s) == str else s)
 
 
-# %%
+
 # convertir duration a dos columnas
 amazon[["duration_int", "duration_type"]] = amazon['duration'].str.split(' ',expand=True)
 disney[["duration_int", "duration_type"]] = disney['duration'].str.split(' ',expand=True)
 hulu[["duration_int", "duration_type"]] = hulu['duration'].str.split(' ',expand=True)
 netflix[["duration_int", "duration_type"]] = netflix['duration'].str.split(' ',expand=True)
+# convertir los valores de duration_int de str a int
+amazon["duration_int"].fillna(0, inplace=True)
+disney["duration_int"].fillna(0, inplace=True)
+hulu["duration_int"].fillna(0, inplace=True)
+netflix["duration_int"].fillna(0, inplace=True)
+amazon["duration_int"].astype(int)
+disney["duration_int"].astype(int)
+hulu["duration_int"].astype(int)
+netflix["duration_int"].astype(int)
 # borrar la columna duration
 amazon.drop('duration', axis=1, inplace=True)
 disney.drop('duration', axis=1, inplace=True)
 hulu.drop('duration', axis=1, inplace=True)
 netflix.drop('duration', axis=1, inplace=True)  
+# igualar nombre de id con los datasets ratings
+amazon.rename(columns={'show_id':'movieId'},inplace=True)
+disney.rename(columns={'show_id':'movieId'},inplace=True)
+hulu.rename(columns={'show_id':'movieId'},inplace=True)
+netflix.rename(columns={'show_id':'movieId'},inplace=True)
 
-# %%
-#Funciones API
-print(amazon.query('release_year == {year} and duration_type == {duration_type} and duration_int == @amazon["duration_int"].max()')["title"].iloc[0])
-    
+#Funciones API    
+df_ratings = pd.concat([df1,df2,df3,df4,df5,df6,df7,df8], ignore_index=True)
+df_amazon = pd.merge(df_ratings, amazon[['movieId','release_year']], on='movieId')
+df_disney = pd.merge(df_ratings, disney[['movieId','release_year']], on='movieId')
+df_hulu = pd.merge(df_ratings, hulu[['movieId','release_year']], on='movieId')
+df_netflix = pd.merge(df_ratings, netflix[['movieId','release_year']], on='movieId')
+
 
 
--- main.py
+++ main.py
@@ -1,10 +1,233 @@
 from fastapi import FastAPI
-from Etl_script import amazon, disney, hulu, netflix
+from Etl_script import amazon, disney, hulu, netflix, df_amazon, df_disney, df_hulu, df_netflix, df_ratings
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import gradio as gr
 app = FastAPI()
 
 
-@app.get("/get_max_duration/{year}/{platform}/{duration_type}")
-async def get_max_duration(year: int, platform: str, duration_type: int):
+@app.get('/get_max_duration/{year}/{platform}/{duration_type}')
+async def get_max_duration(year: int, platform: str, duration_type: str):
     if platform == "amazon":
-        movie = amazon.query('release_year == @year and duration_type == @duration_type and duration_int == @amazon["duration_int"].max()')["title"].iloc[0]
-        return movie
+        movie = amazon.query('release_year == @year and duration_type == @duration_type')
+        
+        titulo = movie.sort_values('duration_int', ascending=False)['title'].iloc[0]
+        
+        return f'La pelicula de amazon del año {str(year)} con más duración en {duration_type} es: {titulo}'
+    
+    elif platform == "disney":
+        
+        movie = disney.query('release_year == @year and duration_type == @duration_type')
+        
+        titulo = movie.sort_values('duration_int', ascending=False)['title'].iloc[0]
+        
+        return f'La pelicula de disney del año {str(year)} con más duración en {duration_type} es: {titulo}'
+    
+    elif platform == "hulu":
+        
+        movie = hulu.query('release_year == @year and duration_type == @duration_type')
+        
+        titulo = movie.sort_values('duration_int', ascending=False)['title'].iloc[0]
+       
+        return f'La pelicula de hulu del año {str(year)} con más duración en {duration_type} es: {titulo}'
+    
+    elif platform == "netflix":
+        
+        movie = netflix.query('release_year == @year and duration_type == @duration_type')
+       
+        titulo = movie.sort_values('duration_int', ascending=False)['title'].iloc[0]
+        
+        return f'La pelicula de netflix del año {year} con más duración en {duration_type} es: {titulo}'
+    
+    else:
+        return 'Plataformas disponibles: "amazon", "disney", "hulu" y "netflix"'
+
+@app.get('/get_score_count/{platform}/{scored}/{year}')
+async def get_score_count(platform: str, scored: float, year: int):
+    if platform == "amazon":
+       
+        df_mean = df_amazon.groupby(['movieId'])['rating'].mean().reset_index()
+        
+        df_final = pd.merge(df_mean, df_amazon[['movieId', 'release_year']], on='movieId')
+       
+        cantidad = df_final.query('rating > @scored and release_year == @year')['movieId'].count()
+       
+        return f'La cantidad de peliculas arriba de {scored} puntos del año {year} son: {cantidad}'
+    elif platform == "disney":
+       
+        df_mean = df_disney.groupby(['movieId'])['rating'].mean().reset_index()
+       
+        df_final = pd.merge(df_mean, df_disney[['movieId', 'release_year']], on='movieId')
+       
+        cantidad = df_final.query('rating > @scored and release_year == @year')['movieId'].count()
+       
+        return f'La cantidad de peliculas arriba de {scored} puntos del año {year} son: {cantidad}'
+    elif platform == "hulu":
+       
+        df_mean = df_hulu.groupby(['movieId'])['rating'].mean().reset_index()
+       
+        df_final = pd.merge(df_mean, df_hulu[['movieId', 'release_year']], on='movieId')
+       
+        cantidad = df_final.query('rating > @scored and release_year == @year')['movieId'].count()
+       
+        return f'La cantidad de peliculas arriba de {scored} puntos del año {year} son: {cantidad}'
+    elif platform == "netflix":
+       
+        df_mean = df_netflix.groupby(['movieId'])['rating'].mean().reset_index()
+       
+        df_final = pd.merge(df_mean, df_netflix[['movieId', 'release_year']], on='movieId')
+       
+        cantidad = df_final.query('rating > @scored and release_year == @year')['movieId'].count()
+       
+        return f'La cantidad de peliculas arriba de {scored} puntos del año {year} son: {cantidad}'
+    else:
+       
+        return 'Plataformas disponibles: "amazon", "disney", "hulu" y "netflix"'
+
+@app.get('/get_count_platform/{platform}')
+async def get_count_platfrom(platform: str):
+    if platform == 'amazon':
+        
+        Cant = amazon.movieId.count()
+        
+        return f'La cantidad de peliculas de {platform} es: {Cant}'
+   
+    elif platform == 'disney':
+        
+        Cant = disney.movieId.count()
+
+        return f'La cantidad de peliculas de {platform} es: {Cant}'
+   
+    elif platform == 'hulu':
+        
+        Cant = hulu.movieId.count()
+
+        return f'La cantidad de peliculas de {platform} es: {Cant}'
+  
+    elif platform == 'netflix':
+        
+        Cant = netflix.movieId.count()
+
+        return f'La cantidad de peliculas de {platform} es: {Cant}'
+    
+    else:
+        
+        return 'Plataformas disponibles: "amazon", "disney", "hulu" y "netflix"'
+
+
+
+@app.get('/get_actor/{platform}/{year}')
+async def get_actor(platform: str, year: int):
+    if platform == 'amazon':
+        
+        df_filtered = amazon[(amazon['release_year'] == year)]
+        
+        actor_list = [actor.strip() for cast in df_filtered['cast'] if not pd.isna(cast) for actor in cast.split(',')]
+        
+        actor_count = {}
+        
+        for actor in actor_list:
+            actor_count[actor] = actor_count.get(actor, 0) + 1
+
+        max_actor = max(actor_count, key=actor_count.get)
+        
+        return f'El actor con más apariciones de {platform} en el año {year} es {max_actor}'
+   
+    elif platform == 'disney':
+        
+        df_filtered = disney[(disney['release_year'] == year)]
+        
+        actor_list = [actor.strip() for cast in df_filtered['cast'] if not pd.isna(cast) for actor in cast.split(',')]
+        
+        actor_count = {}
+        
+        for actor in actor_list:
+            actor_count[actor] = actor_count.get(actor, 0) + 1
+
+        max_actor = max(actor_count, key=actor_count.get)
+        
+        return f'El actor con más apariciones de {platform} en el año {year} es {max_actor}'
+   
+    elif platform == 'hulu':
+        
+        return 'No hay registro de actores es la plataforma de hulu'
+  
+    elif platform == 'netflix':
+        
+        df_filtered = netflix[(netflix['release_year'] == year)]
+        
+        actor_list = [actor.strip() for cast in df_filtered['cast'] if not pd.isna(cast) for actor in cast.split(',')]
+        
+        actor_count = {}
+        
+        for actor in actor_list:
+            actor_count[actor] = actor_count.get(actor, 0) + 1
+
+        max_actor = max(actor_count, key=actor_count.get)
+        
+        return f'El actor con más apariciones de {platform} en el año {year} es {max_actor}'
+    
+    else:
+        
+        return 'Plataformas disponibles: "amazon", "disney", "hulu" y "netflix"'
+
+def get_recommendation(user_id, movie_id):
+    # Obtener los datos de ratings de los usuarios
+    user_ratings = df_ratings[df_ratings['userId'] == user_id]
+    # Obtener los datos de ratings de otros usuarios para la misma película
+    other_ratings = df_ratings[(df_ratings['userId'] != user_id) & (df_ratings['movieId'] == movie_id)]
+    # Unir los datos
+    combined_data = pd.concat([user_ratings, other_ratings], axis=0)
+    # Pivotear los datos para que las filas representen usuarios y las columnas a las películas
+    pivoted_data = combined_data.pivot_table(index='userId', columns='movieId', values='rating').fillna(0)
+    # Entrenar el modelo de K Vecinos más cercanos
+    knn_model = KNeighborsClassifier(n_neighbors=5)
+    knn_model.fit(pivoted_data.values, pivoted_data.index)
+    # Predecir la recomendación para la película y usuario dados
+    user_row = pivoted_data.loc[user_id].values.reshape(1, -1)
+    predicted_rating = knn_model.predict(user_row)
+    if predicted_rating > 3:
+        return 'Recomendada'
+    else:
+        return 'No recomendada'
+
+with gr.Blocks() as demo:
+    gr.Markdown("""
+    # Haga consultas acerca de peliculas con esta demo:
+      Por favor coloque años superiores a 1950
+
+                """)
+    with gr.Tab("Get_max_count"):
+        release_year = gr.Number(label= "Año")
+        platform = gr.Textbox(label="Plataforma", placeholder= "amazon, disney, hulu o netflix...")
+        duration_type = gr.Textbox(label="Tipo de duración", placeholder= "min, season o seasons...")
+        output = gr.Textbox(label="Output")
+        button = gr.Button("Consultar")
+    with gr.Tab("Get_score_count"):
+        platform2 = gr.Textbox(label= "Plataforma",placeholder= "amazon, disney, hulu o netflix...")
+        score = gr.Number(label="Puntaje", placeholder="Puntaje...", float=True)
+        year = gr.Number(label="Año",placeholder= "Desde 1920 en adelante...")
+        output2= gr.Textbox(label="Output")
+        button2 = gr.Button("Consultar")
+    with gr.Tab("Get_count_platform"):
+        platform3 = gr.Textbox(label="Plataforma",placeholder= "amazon, disney, hulu o netflix...")
+        output3 = gr.Textbox(label="Output")
+        button3 = gr.Button("Consultar")
+    with gr.Tab("Get_actor"):
+        platform4 = gr.Textbox(label="Plataforma",placeholder= "amazon, disney, hulu o netflix...")
+        year4 = gr.Number(label="Año")
+        output4 = gr.Textbox(label="Output")
+        button4 = gr.Button("Consultar")
+    with gr.Tab("Get_recommenation"):
+        user_id = gr.Number(label="id de usuario...")
+        movie_id = gr.Textbox(label="id de pelicula...")
+        output5 = gr.Textbox(label="Output")
+        button5 = gr.Button("Predecir")
+    button.click(get_max_duration, inputs=[release_year,platform,duration_type], outputs=output)
+    button2.click(get_score_count, inputs=[platform2,score,year], outputs=output2)
+    button3.click(get_count_platfrom, inputs=platform3, outputs=output3)
+    button4.click(get_actor, inputs=[platform4, year4], outputs=output4)
+    button5.click(get_recommendation, inputs=[user_id,movie_id], outputs=output5)
+    
+demo.launch()
+
